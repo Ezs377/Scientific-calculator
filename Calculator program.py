5e134bda8685aa24c47b55a9dbd3c192ef0b2cdd
Azre <nicercool@gmail.com>
2021-10-21 20:20:31 +1300

    started

--- Calculator program.py
+++ Calculator program.py
@@ -1,1 +1,13 @@
 # Calculator program
+import tkinter, math
+
+class Main:
+    def __init__(self, source):
+        
+    
+    
+    
+root = tkinter.Tk()
+root.title ("Calculator")
+run = Main(root)
+root.mainloop
